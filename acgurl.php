1d0472b04668809f8e7479b54eeb56b9537ba70a
Suxiaoqinx <37541680+Suxiaoqinx@users.noreply.github.com>
2019-04-06 17:07:33 +0800

    Update acgurl.php

--- acgurl.php
+++ acgurl.php
@@ -1,34 +1,44 @@
 <?php
 //读取文本
-$str = explode("\n", file_get_contents('sinetxt.txt'));
+$str = explode("\n", file_get_contents('picimg.txt'));
 $k = rand(0,count($str));
 $sina_img = str_re($str[$k]);
+
 $size_arr = array('large', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb180', 'thumbnail', 'square');
 $size = !empty($_GET['size']) ? $_GET['size'] : 'large' ;
 $server = rand(1,4);
 if(!in_array($size, $size_arr)){
-    $size = 'large';
+	$size = 'large';
 }
 $url = 'https://ws'.$server.'.sinaimg.cn/'.$size.'/'.$sina_img.'.jpg';
 //解析结果
-$result=array("code"=>"200","imgurl"=>"$url");
- 
+$result=array("code"=>"200","acgurl"=>"$url");
 //Type Choose参数代码
 $type=$_GET['return'];
 switch ($type)
 {   
-    
-//Json格式解析
+   
+//格式解析                             
 case 'json':
+$path = "$url";
+$pathinfo = pathinfo($path);
 $imageInfo = getimagesize($url);  
 $result['width']="$imageInfo[0]";  
-$result['height']="$imageInfo[1]";  
+$result['height']="$imageInfo[1]";
+$result['size']="$pathinfo[extension]";    
 header('Content-type:text/json');
-echo json_encode($result);  
+echo json_encode($result);
+break;
+//格式解析                             
+case 'img':
+$img = file_get_contents($url,true);
+//使用图片头输出浏览器
+header("Content-Type: image/jpeg;");
+echo $img;
 break;
 //IMG
 default:
-header("Location:".$result['imgurl']);
+header("Location:".$result['acgurl']);
 break;
 }
 function str_re($str){
