b8b3cfb9fa72e36ce4f0b6cd47532adf8160d29f
Suxiaoqinx <37541680+Suxiaoqinx@users.noreply.github.com>
2020-05-06 11:15:53 +0800

    重写代码

--- acgurl.php
+++ acgurl.php
@@ -1,19 +1,26 @@
 <?php
-//读取文本
-$str = explode("\n", file_get_contents('sinetxt.txt'));
-$k = rand(0,count($str));
-$sina_img = str_re($str[$k]);
-
+// 存储数据的文件
+$filename = 'sinetxt.txt';
+if(!file_exists($filename)) {
+    die($filename . ' 数据文件不存在');
+}
+// 读取整个数据文件
+$data = file_get_contents($filename);
+// 按换行符分割成数组
+$data = explode(PHP_EOL, $data);
+// 随机获取一行索引
+$result = $data[array_rand($data)];
+// 去除多余的换行符（解决获取空值问题）
+$result = str_replace(array("\r","\n","\r\n"), '', $result);
 $size_arr = array('large', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb180', 'thumbnail', 'square');
 $size = !empty($_GET['size']) ? $_GET['size'] : 'large' ;
 $server = rand(1,4);
 if(!in_array($size, $size_arr)){
 	$size = 'large';
 }
-$url = 'http://tva'.$server.'.sinaimg.cn/'.$size.'/'.$sina_img.'.jpg';
+$url = 'https://tva'.$server.'.sinaimg.cn/'.$size.'/'.$result.'.jpg';
 //解析JSON
 $result=array("code"=>"200","acgurl"=>"$url");
-
 $type=$_GET['return'];
 switch ($type)
 {   
@@ -48,12 +55,5 @@ case 'true':
 $url=str_replace("http:","https:", $result['acgurl']);
 header("Location:".$url);
 break;
-}
-function str_re($str){
-  $str = str_replace(' ', "", $str);
-  $str = str_replace("\n", "", $str);
-  $str = str_replace("\t", "", $str);
-  $str = str_replace("\r", "", $str);
-  return $str;
 }
 ?>
