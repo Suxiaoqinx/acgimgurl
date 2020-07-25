bf4951a297773c4ff343314c6148a66b913bffc8
Suxiaoqinx <37541680+Suxiaoqinx@users.noreply.github.com>
2020-07-25 14:57:52 +0800

    更新功能 优化文件代码

--- acgurl.php
+++ acgurl.php
@@ -1,59 +1,61 @@
 <?php
+//API名称
+$APIname='QAC_API';
 // 存储数据的文件
 $filename = 'sinetxt.txt';
 if(!file_exists($filename)) {
-    die($filename . ' 数据文件不存在');
+    die($filename.'数据文件不存在');
+} else {
+	//读取资源文件
+	$giturlArr = file($filename);
 }
-// 读取整个数据文件
-$data = file_get_contents($filename);
-// 按换行符分割成数组
-$data = explode(PHP_EOL, $data);
-// 随机获取一行索引
-$result = $data[array_rand($data)];
-// 去除多余的换行符（解决获取空值问题）
-$result = str_replace(array("\r","\n","\r\n"), '', $result);
-$size_arr = array('large', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb180', 'thumbnail', 'square');
-$size = !empty($_GET['size']) ? $_GET['size'] : 'large' ;
-$server = rand(1,4);
-if(!in_array($size, $size_arr)){
-	$size = 'large';
+$giturlData = [];
+//将资源文件写入数组
+foreach ($giturlArr as $key => $value) {
+	$value = trim($value);
+	if (!empty($value)) {
+		$giturlData[] = trim($value);
+	}
 }
-$url = 'http://tva'.$server.'.sinaimg.cn/'.$size.'/'.$result.'.jpg';
-//解析JSON
-$result=array("code"=>"200","acgurl"=>"$url");
-$type=$_GET['return'];
-switch ($type)
-{   
-//格式解析                             
-case 'json':
-$path = "$url";
-$pathinfo = pathinfo($path);
-$imageInfo = getimagesize($url);  
-$result['width']="$imageInfo[0]";  
-$result['height']="$imageInfo[1]";
-$result['size']="$pathinfo[extension]"; 
-header('Content-type:text/json');
-$url=str_replace("http:","https:", $result);
-echo json_encode($url);
-break;
-//不输出图片链接直接显示                             
-case 'img':
-$img = file_get_contents($url,true);
-header("Content-Type: image/jpeg;");
-echo $img;
-break;
-//IMG
-default:
-header("Location:".$result['acgurl']);
-break;
+//随机输出一张
+$randKey = rand(0, count($giturlData));
+$imgurl = $giturlData[$randKey];
+//随机输出十张图片_后面数字可改
+$randKeys = array_rand($giturlData, 500);
+$imgurls = [];
+foreach ($randKeys as $key) {
+	$imgurls[] = $giturlData[$key];
 }
-$type1=$_GET['ssl'];
-switch ($type1)
-{   
-//HTTPS图片输出                             
-case 'true':
-$url=str_replace("http:","https:", $result['acgurl']);
-header("Location:".$url);
-break;
+//json格式
+$json = array("API_name"=>"$APIname");
+$returnType = $_GET['return'];
+switch ($returnType) {
+	//浏览器直接输出图片
+	case 'img':
+		$img = file_get_contents($imgurl, true);
+		header("Content-Type: image/jpeg;");
+		echo $img;
+		break;
+	//随机JSON输出10张图片
+	case 'jsonpro':
+		header('Content-type:text/json');
+		//随机输出十张
+		case 'jsonpro':
+		$json['imgurls'] = $imgurls;
+		echo json_encode($json,JSON_PRETTY_PRINT);
+		break;
+	//JSON格式输出
+	case 'json':
+		$json['imgurl'] = $imgurl;
+		$imageInfo = getimagesize($imgurl);
+		$json['width'] = "$imageInfo[0]";
+		$json['height'] = "$imageInfo[1]";
+		header('Content-type:text/json');
+		echo json_encode($json,JSON_PRETTY_PRINT);
+		break;
+    //直接跳转		
+	default:
+		header("Location:" . $imgurl);
+		break;
 }
 ?>
